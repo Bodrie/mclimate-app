77237c8dd60d2fdc52e0f568adc8c0632bee63c1
Dobromir Kirov <kirov0407@gmail.com>
2023-09-03 14:40:53 +0300

    created the building dashboard page

---
+++ src/pages/DashboardPage/ContentNav/ContentNav.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Button, Navigation, Text } from "../../../components";
+import { useNavigate } from "react-router-dom";
+import img from "../../../assets/add-a-floor.png";
+
+interface ContentNavProps {
+  currentBuildingInfo: any;
+}
+
+const ContentNav = ({ currentBuildingInfo }: ContentNavProps) => {
+  const navigate = useNavigate();
+  return (
+    <Navigation
+      tabs={[
+        {
+          id: 0,
+          name: "All Devices",
+          el: <div className="dashboard-nav-panels">No Information</div>,
+        },
+        {
+          id: 1,
+          name: "Apartments",
+          el: <div className="dashboard-nav-panels">No Information</div>,
+        },
+        {
+          id: 2,
+          name: "Floors",
+          el: (
+            <div className="dashboard-nav-panels">
+              <div className="dashboard-tab-floor">
+                <div className="create-floor">
+                  <Text textColor="#51545a">
+                    Floors: ({currentBuildingInfo.floors.length.toString()})
+                  </Text>
+                  <Button
+                    text="Create new floor"
+                    variant="primary"
+                    icon="plus-icon"
+                    iconColor="#fff"
+                    iconSize={15}
+                  />
+                </div>
+                <div className="create-floor-content">
+                  <img
+                    src={img}
+                    alt="Add a floor"
+                    onClick={() => navigate("/dashboard/floors")}
+                  />
+                  <Text textColor="#77758b">
+                    There are no floors created for this building.
+                  </Text>
+                </div>
+              </div>
+            </div>
+          ),
+        },
+        {
+          id: 3,
+          name: "Rooms",
+          el: <div className="dashboard-nav-panels">No Information</div>,
+        },
+        {
+          id: 4,
+          name: "Manage Devices",
+          el: <div className="dashboard-nav-panels">No Information</div>,
+        },
+        {
+          id: 5,
+          name: "UserManagement",
+          el: <div className="dashboard-nav-panels">No Information</div>,
+        },
+      ]}
+    />
+  );
+};
+
+export default ContentNav;
--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,15 +1,53 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { buildings } from "../../mock/buildings";
+import { Button, Heading, Card, Text } from "../../components";
+import ContentNav from "./ContentNav/ContentNav";
+import "./DashboardPage.css";
 
 const DashboardPage = () => {
+  const navigate = useNavigate();
   const { state } = useLocation();
   const { buildingId } = state;
   const currentBuilding = buildings.find(
     (currentElement) => currentElement.id === buildingId
   );
+  if (typeof currentBuilding === "undefined") return <></>;
 
-  return <>DashboardPage</>;
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-head-panel">
+        <Button
+          text="BACK"
+          variant="secondary"
+          icon="arrow-left"
+          iconColor="#77758b"
+          iconSize={12}
+          onClick={() => navigate("/home")}
+        />
+        <Button text="Edit building" variant="primary" />
+      </div>
+      <Heading variant={2} align="left" color="#51545a">
+        Building dashboard
+      </Heading>
+      <Card content={currentBuilding} />
+      <div className="dashboard-devices">
+        <div className="dashboard-devices-panel">
+          <Text textColor="#77758b">Online Devices</Text>
+          <Text textColor="#77758b" icon="dot" iconColor="green" iconSize={15}>
+            {currentBuilding.onlineDevices.length.toString()}
+          </Text>
+        </div>
+        <div className="dashboard-devices-panel">
+          <Text textColor="#77758b">Total Devices</Text>
+          <Text textColor="#77758b">
+            {currentBuilding.devices.length.toString()}
+          </Text>
+        </div>
+      </div>
+      <ContentNav currentBuildingInfo={currentBuilding} />
+    </div>
+  );
 };
 
 export default DashboardPage;
